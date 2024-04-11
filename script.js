34e79aa87d6fa992a2d33c55fa14781fd0a614f3
sridharaniruddha <152184230+sridharaniruddha@users.noreply.github.com>
2024-04-11 12:55:08 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// deals the player a random number between 4 and 21 inclusive 
+// if the player has 21, they win
+
+//Deals a random number between 2 and 11 inclusive to the dealer 
+// if the player "hits", add a number between 2 and 11
+// if the player goes over 21, they lose 
+
+var game = {
+    dealer: 0,
+    player: 0,
+    game: 21,
+    isPlayerTurn: false,
+};
+
+function blackjack() {
+    //assgin number between 4 and 21 inclusive
+}
+
+
+function gameplay (playerTotal, dealerTotal) {
+    
+}
